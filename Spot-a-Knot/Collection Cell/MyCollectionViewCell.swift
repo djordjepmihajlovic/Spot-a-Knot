977d881384e9d130741e90fb86674d0cc96d390e
Djordje Mihajlovic <djdjdjordje@gmail.com>
2023-10-01 23:37:14 +0100

    Knot Collection setup

---
+++ Spot-a-Knot/Collection Cell/MyCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MyCollectionViewCell.swift
+//  Spot-a-Knot
+//
+//  Created by Djordje Mihajlovic on 01/10/2023.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var myLabel: UILabel!
+    @IBOutlet var myImageView: UIImageView!
+    
+    static let identifier = "MyCollectionViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "MyCollectionViewCell", bundle: nil)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    public func configure(with model:Model) {
+        self.myLabel.text = model.text
+        self.myImageView.image = UIImage(named: model.imageName)
+    }
+
+}
--- Spot-a-Knot/CollectionViewController.swift
+++ Spot-a-Knot/CollectionViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class CollectionViewController: UIViewController {
+class CollectionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    var models = [Model]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        table.register(CollectionTableViewCell.nib(), forCellReuseIdentifier: CollectionTableViewCell.identifier)
+        table.delegate = self
+        table.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: CollectionTableViewCell.identifier, for: indexPath) as! CollectionTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250.0
+    }
+    
 
+}
+
+struct Model {
+    let text: String
+    let imageName: String
+    
+    init(text: String, imageName: String){
+        self.text = text
+        self.imageName = imageName
+    }
 }
